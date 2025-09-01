51f734556ef29a8502d2b42687225d06136f35da
MottoMotto <alessandro.aoc@gmail.com>
2025-09-01 15:16:33 -0400

    Criado tex Form Fiel customizado

---
+++ lib/componentes/tex_form_field_custom.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  final TextEditingController? controller;
+  final EdgeInsetsGeometry? padding;
+  final String? hintText;
+  final String? labelText;
+  final FormFieldValidator<String>? validator;
+  final String? helperText;
+
+  const CustomTextField({
+    super.key,
+    this.controller,
+    this.padding,
+    this.hintText,
+    this.labelText,
+    this.validator,
+    this.helperText,
+  });
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  final defaultBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10.0),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      validator: widget.validator,
+      //keyboardType: widget.emailType,
+      decoration: InputDecoration(
+        helperMaxLines: 3,
+        labelText: widget.labelText,
+        hintText: widget.hintText,
+        focusedBorder: defaultBorder,
+        //coloca uma cor na borda e no texto de aviso em caso de error
+        errorBorder: defaultBorder.copyWith(
+          borderSide: const BorderSide(color: Colors.red),
+        ),
+        focusedErrorBorder: defaultBorder,
+        enabledBorder: defaultBorder,
+        disabledBorder: defaultBorder,
+        helperText: widget.helperText,
+      ),
+    );
+  }
+}
