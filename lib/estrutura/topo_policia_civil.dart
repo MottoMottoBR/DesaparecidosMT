0651cc70fec931856844aca88cd6a8d28be3cdca
MottoMotto <alessandro.aoc@gmail.com>
2025-09-07 17:48:46 -0400

    ajustes no Layout da pagina

--- lib/estrutura/topo_policia_civil.dart
+++ lib/estrutura/topo_policia_civil.dart
@@ -14,41 +14,53 @@ class TopoPoliciaCivil extends StatelessWidget {
           colors: [Colors.black, Colors.black87, Colors.black45],
         ),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Wrap(
         children: [
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                Image.asset("lib/imagens/logocivil.png", width: 80, height: 80),
-                Column(
-                  children: [
-                    Text(
-                      StringTxT.Titulo,
-                      style: GoogleFonts.adamina(
-                        textStyle: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                    ),
-                    Text(
-                      StringTxT.Titulo2,
-                      style: GoogleFonts.adamina(
-                        textStyle: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Icon(Icons.person_2, color: Colors.white),
-              Text(
-                StringTxT.Titulo3,
-                style: TextStyle(color: Colors.white, fontSize: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    "lib/imagens/logocivil.png",
+                    width: 80,
+                    height: 80,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        StringTxT.Titulo,
+                        style: GoogleFonts.adamina(
+                          textStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        StringTxT.Titulo2,
+                        style: GoogleFonts.adamina(
+                          textStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              SizedBox(width: 20),
+              Row(
+                children: [
+                  Icon(Icons.person_2, color: Colors.white),
+                  Text(
+                    StringTxT.Titulo3,
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  SizedBox(width: 20),
+                ],
+              ),
             ],
           ),
         ],
