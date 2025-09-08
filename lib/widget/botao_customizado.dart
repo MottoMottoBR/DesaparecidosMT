d7d82c49fbae43115a9ce5e1796893ea406b5eda
MottoMottoBR <alessandro.aoc@gmail.com>
2025-09-08 17:26:50 -0400

    Adicionado GestureDetector dentro do componente para ser reutilizado

--- lib/widget/botao_customizado.dart
+++ lib/widget/botao_customizado.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class CustomBotao extends StatefulWidget {
   final String text;
   final Color color;
+  final VoidCallback? onPressed;
 
   final icon;
   const CustomBotao({
@@ -10,6 +11,7 @@ class CustomBotao extends StatefulWidget {
     required this.text,
     required this.color,
     required this.icon,
+    this.onPressed,
   });
 
   @override
@@ -19,17 +21,23 @@ class CustomBotao extends StatefulWidget {
 class _CustomBotaoState extends State<CustomBotao> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: widget.color,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(widget.icon, color: Colors.white),
-          Text(widget.text, style: TextStyle(color: Colors.white)),
-        ],
+    return GestureDetector(
+      onTap: widget.onPressed,
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: Container(
+          decoration: BoxDecoration(
+            color: widget.color,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(widget.icon, color: Colors.white),
+              Text(widget.text, style: TextStyle(color: Colors.white)),
+            ],
+          ),
+        ),
       ),
     );
   }
