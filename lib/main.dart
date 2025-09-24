6d92111c94009560f653f83266746f848dc50c14
MottoMottoBR <alessandro.aoc@gmail.com>
2025-09-24 13:42:43 -0400

    Ajuste no Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/paginas/home_page.dart';
+import 'package:myapp/paginas/pessoa_detalhes.dart';
 import 'package:myapp/ultis/strings.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: StringTxT.TituloBarra,
-      home: const HomePage(),
+      home: const PessoaDetalhes(),
     );
   }
 }
--- lib/paginas/pessoa_detalhes.dart
+++ lib/paginas/pessoa_detalhes.dart
@@ -10,27 +10,70 @@ class PessoaDetalhes extends StatefulWidget {
 class _PessoaDetalhesState extends State<PessoaDetalhes> {
   @override
   Widget build(BuildContext context) {
-    return Wrap(
+    // Pega a largura da tela para decidir o layout
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    // Define um breakpoint para a mudança de layout
+    const double breakpoint = 800.0;
+
+    // Retorna um Column para telas menores e um Row para telas maiores
+    Widget layoutChildren = screenWidth < breakpoint
+        ? Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center, // Centraliza horizontalmente os filhos
       children: [
         Container(
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
+            borderRadius: BorderRadius.circular(3),
             color: Colors.red,
           ),
           width: 500.0,
           height: 500.0,
-          child: Center(child: Icon(Icons.person)),
+          child: const Center(child: Icon(Icons.person)),
         ),
+
         Container(
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
+            borderRadius: const BorderRadius.only(
+              bottomRight: Radius.circular(3),
+              topRight: Radius.circular(3),
+            ),
             color: Colors.blue,
           ),
-          width: 500.0,
+          width: 300.0,
           height: 25.0,
-          child: Center(child: Icon(Icons.person)),
+          child: const Center(child: Icon(Icons.person)),
+        ),
+      ],
+    )
+        : Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(3),
+            color: Colors.red,
+          ),
+          width: 500.0,
+          height: 500.0,
+          child: const Center(child: Icon(Icons.person)),
+        ),
+
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.only(
+              bottomRight: Radius.circular(3),
+              topRight: Radius.circular(3),
+            ),
+            color: Colors.blue,
+          ),
+          width: 300.0,
+          height: 25.0,
+          child: const Center(child: Icon(Icons.person)),
         ),
       ],
     );
+
+    return Center(child: layoutChildren);
   }
 }
