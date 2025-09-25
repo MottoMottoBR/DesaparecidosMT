257072c9f43ef8e892cc30e73c481cfb5ced21d7
MottoMottoBR <alessandro.aoc@gmail.com>
2025-09-25 15:10:33 -0400

    Adicionado: Adicionado mais Strings

--- lib/ultis/strings.dart
+++ lib/ultis/strings.dart
@@ -10,6 +10,19 @@ class StringTxT {
   static const String Titulo3 = 'Pessoas Desaparecidas';
   static const String TituloBarra = 'Desaparecidos PJC-MT';
 
-  //Pessoas Detalhes
-  static const String textDesaparecido = 'Desaparecido';
+  static const String textoDesaparecido = 'Desaparecido';
+  static const String pessoaNome = 'UserName';
+  static const int pessoaIdade = 0;
+  static const String pessoaSexo = 'Feminio ou Masculino ';
+  static const String subTitulo = 'Dados sobre o Desaparecimento';
+  static const String localDesaparecimento = 'Desconhecido';
+  static const String vestimenta = 'Desconhecido';
+  static const String infoAdicionais = 'Desconhecido';
+  static const String ajudaCompartilhando = 'Ajude compartilhando';
+  static String tempoDesaparecido = 'DESAPARECIDA HÁ{meses} MES!';
+  static String viuEssaPessoa = 'Viu ou sabe desssa Pessoa?';
+  static String ajudeCompartilhando = 'Ajude compartilhando:';
+
+  //Declacara a data como String
+  static const String dtDesaparecimento = '2025-02-14T12:00:00';
 }
