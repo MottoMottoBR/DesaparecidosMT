ec0456e235466784502eece55b0fbbf4c55e43ba
MottoMottoBR <alessandro.aoc@gmail.com>
2025-09-26 14:51:02 -0400

    criado para gerenciar os stylos de letras

---
+++ lib/ultis/app_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  AppStyles._();
+
+  static const TextStyle textoDesaparecidoStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+}
