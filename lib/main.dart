289cb4bb05711a161325767153096cc235e9c2a8
MottoMottoBR <alessandro.aoc@gmail.com>
2026-02-05 13:16:51 -0400

    Adicionado: Forçado "bad certificate" modo global

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/paginas/home_page.dart';
-import 'package:myapp/paginas/pessoa_detalhes.dart';
 import 'package:myapp/ultis/strings.dart';
+import 'dart:io';
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
